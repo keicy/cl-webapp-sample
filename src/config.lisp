1ab011436aa516c4d02e61f159cb40db94600095
keicy <kei.nakamura97@gmail.com>
2015-04-11 10:28:47 +0900

    Refactor config.lisp.

--- src/config.lisp
+++ src/config.lisp
@@ -20,7 +20,10 @@
 (defparameter *template-directory* (merge-pathnames #P"templates/" *application-root*))
 
 (defconfig :common
-    `(:databases ((:maindb :mysql :database-name "mytest" :username "mysql" :passqord "mysql"))))
+    `(:databases ((:maindb :mysql 
+                   :database-name "mytest" 
+                   :username "mysql" 
+                   :passqord "mysql"))))
 
 (defconfig |development|
   '())
